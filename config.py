4bc7a6f657989f0f3390de2568334586ea0f95ff
ying.tan <ying.tan@tcl.com>
2014-09-10 11:33:38 +0800

    internal release for Porting checker

--- config.py
+++ config.py
@@ -8,3 +8,4 @@ LOG_LEVEL = logging.DEBUG
 LOG_LEVEL_FILE = logging.DEBUG
 LOG_FILE = 'PortingChecker.log'
 STANDARD_FILE = 'standard.json'
+RESULT_FILE = 'compareResult.txt'
--- utils.py
+++ utils.py
@@ -6,16 +6,19 @@ import os
 import sys
 import log
 import json
+import config
 import commands
 
 logger = log.getLogging('utils.py')
+
+def compare_string(s1, s2):
+    return (s1 == s2) and True or False
 
 def get_standard_pkg_config():
     fIn = open(config.STANDARD_FILE, 'rb')
     text = fIn.read()
     fIn.close()
     standard_pkg_dict = json.loads(text)
-    logger.debug(standard_pkg_dict)
     return standard_pkg_dict
 
 def get_path_from_phone():
@@ -24,12 +27,16 @@ def get_path_from_phone():
     if os.path.isfile('apkList.txt'):
         path_dict = get_pkg_path_dict()
         return path_dict
+    else:
+        sys.exit(0)
 
 def get_version_from_phone(pkg_name):
     command = 'adb shell pm dump %s|grep versionName'
     status, output = commands.getstatusoutput(command % pkg_name)
-    logger.debug(output.strip())
-    return output.strip()
+    versionName = output.strip()
+    index = versionName.find('=') + 1
+    version = versionName[index:]
+    return version
 
 def get_pkg_path_dict():
     pkg_dict = {}
@@ -39,9 +46,12 @@ def get_pkg_path_dict():
         if not line:
             break
         else:
-
-            row = line.split('=')
-            name = row[1].strip('\r\n')
+            if line.find('=') > -1:
+                row = line.split('=')
+                name = row[1].strip('\r\n')
+            else:
+                row = line.strip('\r\n')
+                name = ''
 
             #strip 'package:'
             index = row[0].find(':') + 1
@@ -57,3 +67,10 @@ class Info(object):
         return self.message
     def show(self):
         print self.message 
+
+
+class StandardJsonError(Exception):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return self.message 
