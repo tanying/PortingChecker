612fa994b798f10027017dbc658e25b67e0adee4
ying.tan <ying.tan@tcl.com>
2014-09-15 17:05:40 +0800

    fix bug

--- js/generater.js
+++ js/generater.js
@@ -9,7 +9,7 @@ $(function() {
 function addApk(){
 
     console.log($('#pkg' + i).attr('value'))
-    $('#apkInfo').append('<p id="apk'+i+'">Package name: <input id="pkg'+i+'value="" maxlength="256"></p>');
+    $('#apkInfo').append('<p id="apk'+i+'">Package name: <input id="pkg'+i+'" value="" maxlength="256"></p>');
     $('#apk' + i).after('<span>path:<input value="" maxlength="256"></span><br><span>version:<input value="" maxlength="256"></span><br>');
     i++;
 }
