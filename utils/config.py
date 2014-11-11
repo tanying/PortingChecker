fbac3beb8fe9d0490ba7c72dd76f23ce03daa477
ying.tan <ying.tan@tcl.com>
2014-11-11 16:47:50 +0800

    add sign list

--- utils/config.py
+++ utils/config.py
@@ -12,6 +12,7 @@ STANDARD_3RD_FILE = 'config/3rd.json'
 STANDARD_GMS_FILE = 'config/gms.json'
 STANDARD_JSON_CONFIG_JS = 'js/jsonConfig.js'
 RESULT_FILE =  OUTPUT_DIR + 'compareResult.txt'
+SIGN_RESULT = OUTPUT_DIR + 'signResult.txt'
 PULL_PATH_FILE = 'utils/PullApkInfoFromPhone.sh'
 PATH_FILE = OUTPUT_DIR + 'apkList.txt'
 CONFIG_SET = ['gms.json', '3rd.json']
